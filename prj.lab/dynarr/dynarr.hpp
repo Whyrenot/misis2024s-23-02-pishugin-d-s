4b9ddf13ff64a385e64268d956799ba08ba9c975
Whyrenot <dmitrypi380@gmail.com>
2024-02-18 19:42:13 +0300

    dynarr capacity and operator[] fixed

--- prj.lab/dynarr/dynarr.hpp
+++ prj.lab/dynarr/dynarr.hpp
@@ -3,23 +3,32 @@
 #define DYNARR_DYNARR_HPP_20231203
 
 #include <cstddef>
+#include <iostream>
+#include <sstream>
 #include <algorithm>
+
+
+
 
 class DynArr {
 public:
-	[[nodiscard]] DynArr() = default;
-	[[nodiscard]] DynArr(const DynArr&) /*= default*/;
-	[[nodiscard]] DynArr(const std::ptrdiff_t size) : size_(size) { }
-	~DynArr() /*= default*/;
-	[[nodiscard]] DynArr& operator=(const DynArr&) /*= default*/;
+    DynArr() = default;
+    DynArr(const DynArr& d);
+    DynArr(std::ptrdiff_t size);
+    ~DynArr() { delete[] data_; }
 
-	[[nodiscard]] std::ptrdiff_t Size() const noexcept { return size_; }
-	void Resize(const std::ptrdiff_t size) { size_ = size; }
-	/*[[nodiscard]] float& operator[](const std::ptrdiff_t idx) { return data_; }
-	[[nodiscard]] const float& operator[](const std::ptrdiff_t idx) const { return data_; }*/
+    [[nodiscard]] DynArr& operator=(const DynArr& d) noexcept;
+
+    [[nodiscard]] std::ptrdiff_t size() const noexcept;
+    void resize(std::ptrdiff_t size);
+
+    [[nodiscard]] float& operator[](const std::ptrdiff_t i);
+    [[nodiscard]] const float& operator[](const std::ptrdiff_t i) const;
+
 private:
-	std::ptrdiff_t size_ = 0;
-	float *data_ = nullptr;
-};
+    std::ptrdiff_t size_ = 0;
+    std::ptrdiff_t capacity_ = 0;
 
+    float* data_ = nullptr;
+};
 #endif
--- prj.test/test_dynarr.cpp
+++ prj.test/test_dynarr.cpp
@@ -5,21 +5,21 @@
 
 TEST_CASE("dynarr ctor") {
   DynArr arr_def;
-  CHECK_EQ(arr_def.Size(), 0);
+  CHECK_EQ(arr_def.size(), 0);
 
   const int size = 5;
   DynArr arr_s(size);
-  CHECK_EQ(arr_s.Size(), size);
+  CHECK_EQ(arr_s.size(), size);
 
   CHECK_THROWS(void(DynArr(0)));
   CHECK_THROWS(void(DynArr(-size)));
 }
 
-//TEST_CASE("dynarr op[]") {
-//  const int size = 5;
-//  DynArr arr(size);
-//  CHECK_EQ(arr[0], 0);
-//  CHECK_EQ(arr[arr.Size() - 1], 0);
-//  CHECK_THROWS(arr[-1]);
-//  CHECK_THROWS(arr[arr.Size()]);
-//}
+TEST_CASE("dynarr op[]") {
+  const int size = 5;
+  DynArr arr(size);
+  CHECK_EQ(arr[0], 0);
+  CHECK_EQ(arr[arr.size() - 1], 0);
+  CHECK_THROWS(arr[-1]);
+  CHECK_THROWS(arr[arr.size()]);
+}
